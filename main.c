c2aaeaf99214d8e1cb09dc65270f92ea2c8ed288
git <65435748@qq.com>
2019-01-15 19:49:51 +0800

    构建应用功能框架

---
+++ main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main()
+{
+
+    printf("敏感字符管理系统\n");
+
+    while (1)
+    {
+        printf("1---添加敏感字符\n");
+        printf("2---删除敏感字符（最后一个）\n");
+        printf("3---查看所有敏感字符\n");
+        printf("4---替换内容中的敏感字符\n");
+        printf("5---退出\n");
+
+        int code;
+        printf("请选择相应的功能\n");
+        scanf("%d", &code);
+
+        if (code == 1)
+        {
+        }
+        if (code == 2)
+        {
+        }
+        if (code == 3)
+        {
+        }
+        if (code == 4)
+        {
+        }
+        if (code == 5)
+        {
+        }
+    }
+
+    return 0;
+}
